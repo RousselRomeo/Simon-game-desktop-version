59f105d93a0f3395951ae819515389aa86531551
RousselRomeo <50556509+RousselRomeo@users.noreply.github.com>
2020-04-12 00:03:59 +0200

    removed sound path

--- game.js
+++ game.js
@@ -75,7 +75,7 @@ function animatePress(currentColor) {
 }
 //plays corresponding sounds of buttons
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio( name + ".mp3");
   audio.play();
 }
 
